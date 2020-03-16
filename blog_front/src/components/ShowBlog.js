0618be8aa159fc259f7bbafd7bf01de876d611b1
Vince <user@Users-Air.fios-router.home>
2020-03-16 09:36:59 -0400

    final touches

--- blog_front/src/components/ShowBlog.js
+++ blog_front/src/components/ShowBlog.js
@@ -6,7 +6,6 @@ class ShowBlog extends React.Component {
       <>
         <div className="details">
          <h3>Blog Title: {this.props.blog.title}</h3>
-
          <h4>Entry: {this.props.blog.entry}</h4>
          <h6><span>Likes:</span></h6>
        </div>
